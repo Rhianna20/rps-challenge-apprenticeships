1ca5940619f0240a750df246668f7d3e3b9b0388
Rhianna <rhiannastuart@hotmail.co.uk>
2020-10-30 11:40:47 +0000

    submit works

--- app.rb
+++ app.rb
@@ -3,10 +3,19 @@ require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
   get '/' do
-    erb :index
+    erb(:index)
   end
 
   run! if app_file == $0
+
+  get '/name' do
+   @name = params[:name]
+   redirect :play
+  end
+
+  post '/play' do
+    erb(:play)
+end
 
 
 end
